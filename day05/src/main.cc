857dcac646e9f1653ead09be5ac7bb5f530bd152
David Stoss <dstoss17@vt.edu>
2021-11-26 16:11:00 -0500

    Setup all day directories

---
+++ day05/src/main.cc
@@ -0,0 +1,29 @@
+/*
+ * main.cc
+ * Copyright (C) 2021 dstoss <dstoss17@vt.edu>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "impl.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "aoc/parts.h"
+#include "aoc/utils.h"
+
+namespace aoc {
+
+std::string part1(std::istream& in, bool verbose) {
+  std::ostringstream out;
+  out << "Not implemented";
+  return out.str();
+}
+
+std::string part2(std::istream& in, bool verbose) {
+  std::ostringstream out;
+  out << "Not implemented";
+  return out.str();
+}
+
+} // namespace aoc
---
+++ day07/include/impl.h
@@ -0,0 +1,12 @@
+/*
+ * impl.h
+ * Copyright (C) 2021 dstoss <dstoss17@vt.edu>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef ADVENT_OF_CODE_2021_IMPL_H
+#define ADVENT_OF_CODE_2021_IMPL_H
+
+
+#endif // ADVENT_OF_CODE_2021_IMPL_H
