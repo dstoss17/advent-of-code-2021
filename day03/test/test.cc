770bd72285fb6c69e4f1270952d9c102bd026207
David Stoss <dstoss17@vt.edu>
2021-12-03 17:52:18 -0500

    Finish Day 3

--- day03/test/test.cc
+++ day03/test/test.cc
@@ -18,15 +18,28 @@ constexpr bool kVerbose = true;
 constexpr bool kVerbose = false;
 #endif
 
+std::string example = "00100\n"
+                      "11110\n"
+                      "10110\n"
+                      "10111\n"
+                      "10101\n"
+                      "01111\n"
+                      "00111\n"
+                      "11100\n"
+                      "10000\n"
+                      "11001\n"
+                      "00010\n"
+                      "01010";
+
 TEST(Day03, ExampleOnePartOne) {
-  std::string input = "1122";
+  std::string input = example;
   std::istringstream in(input);
-  EXPECT_EQ(aoc::part1(in, kVerbose), "Not implemented");
+  EXPECT_EQ(aoc::part1(in, kVerbose), "198");
 }
 
 TEST(Day03, ExampleOnePartTwo) {
-  std::string input = "1122";
+  std::string input = example;
   std::istringstream in(input);
-  EXPECT_EQ(aoc::part2(in, kVerbose), "Not implemented");
+  EXPECT_EQ(aoc::part2(in, kVerbose), "230");
 }
 
