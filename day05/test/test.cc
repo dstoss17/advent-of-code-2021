11c84de9bcdddc11d96dbc1591d4603f468bddb6
David Stoss <dstoss17@vt.edu>
2021-12-05 16:12:33 -0500

    Day 5 complete

--- day05/test/test.cc
+++ day05/test/test.cc
@@ -18,15 +18,26 @@ constexpr bool kVerbose = true;
 constexpr bool kVerbose = false;
 #endif
 
+std::string example = "0,9 -> 5,9\n"
+                      "8,0 -> 0,8\n"
+                      "9,4 -> 3,4\n"
+                      "2,2 -> 2,1\n"
+                      "7,0 -> 7,4\n"
+                      "6,4 -> 2,0\n"
+                      "0,9 -> 2,9\n"
+                      "3,4 -> 1,4\n"
+                      "0,0 -> 8,8\n"
+                      "5,5 -> 8,2";
+
 TEST(Day05, ExampleOnePartOne) {
-  std::string input = "1122";
+  std::string input = example;
   std::istringstream in(input);
-  EXPECT_EQ(aoc::part1(in, kVerbose), "Not implemented");
+  EXPECT_EQ(aoc::part1(in, kVerbose), "5");
 }
 
 TEST(Day05, ExampleOnePartTwo) {
-  std::string input = "1122";
+  std::string input = example;
   std::istringstream in(input);
-  EXPECT_EQ(aoc::part2(in, kVerbose), "Not implemented");
+  EXPECT_EQ(aoc::part2(in, kVerbose), "12");
 }
 
