f45b85eaa098e0cf72b02ad33a1e58a208b87d24
David Stoss <dstoss17@vt.edu>
2021-12-16 22:27:05 -0500

    complete day 7

--- day07/src/main.cc
+++ day07/src/main.cc
@@ -26,6 +26,23 @@ std::vector<int> read_positions(std::istream& in)
   return positions;
 }
 
+int fuel_cost(int position, int location, bool linear)
+{
+  int distance = std::abs(position - location);
+  if (linear) {
+    return distance;
+  }
+  else {
+    return ((distance + 1) * distance) / 2;
+  }
+}
+
+int compute_fuel_cost(const std::vector<int>& positions, int location, bool linear)
+{
+  auto cost = [location, linear](int sum, int pos) { return sum + fuel_cost(pos, location, linear); };
+  return std::accumulate(positions.begin(), positions.end(), 0, cost);
+}
+
 int find_median(std::vector<int> positions)
 {
   std::sort(positions.begin(), positions.end());
@@ -46,17 +63,22 @@ int find_mean(std::vector<int> positions)
   return static_cast<int>(std::round(sum / positions.size()));
 }
 
-int compute_fuel_cost(int pos, const std::vector<int>& positions, bool part2)
+int find_best_cost(const std::vector<int>& positions, bool linear)
 {
-  int total_cost = 0;
-  for (int p : positions) {
-    int distance = std::abs(pos - p);
-    int cost = (part2) ? ((distance + 1) * distance) / 2 : distance;
+  int max_locations = *std::max_element(positions.begin(), positions.end());
 
-    total_cost += cost;
+  int c1 = compute_fuel_cost(positions, 0, linear);
+
+  for (int location = 1; location < max_locations; location++) {
+    int c2 = compute_fuel_cost(positions, location, linear);
+    if (c2 > c1) {
+      break;
+    }
+
+    c1 = c2;
   }
 
-  return total_cost;
+  return c1;
 }
 
 namespace aoc {
@@ -67,9 +89,8 @@ std::string part1(std::istream& in, bool verbose) {
     std::cout << pos;
   }
 
-  int min_pos = find_median(pos);
   std::ostringstream out;
-  out << compute_fuel_cost(min_pos, pos, false);
+  out << find_best_cost(pos, true);
   return out.str();
 }
 
@@ -79,9 +100,8 @@ std::string part2(std::istream& in, bool verbose) {
     std::cout << pos;
   }
 
-  int min_pos = find_mean(pos);
   std::ostringstream out;
-  out << compute_fuel_cost(min_pos, pos, true);
+  out << find_best_cost(pos, false);
   return out.str();
 }
 
