9926b9fa9d814d7e234c51718a0ca78bfc205414
David Stoss <dstoss17@vt.edu>
2021-12-17 15:33:23 -0500

    day 8 part 1 complete

--- day08/src/main.cc
+++ day08/src/main.cc
@@ -9,14 +9,106 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <bitset>
+#include <array>
 #include "aoc/parts.h"
 #include "aoc/utils.h"
+
+using Segment = std::bitset<7>;
+const std::array<Segment, 10> numbers {
+// order:  abcdefg
+  Segment("1110111"),  // 0
+  Segment("0010010"),  // 1
+  Segment("1011101"),  // 2
+  Segment("1011011"),  // 3
+  Segment("0111010"),  // 4
+  Segment("1101011"),  // 5
+  Segment("1101111"),  // 6
+  Segment("1010010"),  // 7
+  Segment("1111111"),  // 8
+  Segment("1111011"),  // 9
+};
+
+Segment str_to_segment(const std::string& str)
+{
+  Segment result; // defaults to all 0
+
+  for (char c : str) {
+    // note: order is "reversed" (abcdefg, not gfedcba)
+    size_t bit = 'g' - c;
+    result.set(bit);
+  }
+
+  return result;
+}
+
+struct Display {
+  std::array<Segment, 10> patterns;
+  std::array<Segment, 4> outputs;
+};
+
+std::istream& operator>>(std::istream& in, Display& display)
+{
+  std::string tmp;
+  for (auto& pattern : display.patterns) {
+    in >> tmp;
+    pattern = str_to_segment(tmp);
+  }
+
+  in.ignore(3);
+  for (auto& output : display.outputs) {
+    in >> tmp;
+    output = str_to_segment(tmp);
+  }
+
+  return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const Display& display)
+{
+  out << "patterns:";
+  for (const auto& pattern : display.patterns) {
+    out << ' ' << pattern;
+  }
+  out << " - outputs:";
+  for (const auto& output : display.outputs) {
+    out << ' ' << output;
+  }
+
+  return out;
+}
+
+int count_unique(const std::vector<Display>& displays, bool verbose)
+{
+  // count the number of outputs with 2, 3, 4, or 7 segments enabled
+  int count = 0;
+
+  for (const auto& display : displays) {
+    if (verbose) {
+      std::cout << display << '\n';
+    }
+
+    for (auto output : display.outputs) {
+      size_t n = output.count();
+      if (n == 2 || n == 3 || n == 4 || n == 7) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
 
 namespace aoc {
 
 std::string part1(std::istream& in, bool verbose) {
+  auto displays = read_in<Display>(in);
+  if (verbose) {
+    std::cout << displays;
+  }
+
   std::ostringstream out;
-  out << "Not implemented";
+  out << count_unique(displays, verbose);
   return out.str();
 }
 
--- day08/test/test.cc
+++ day08/test/test.cc
@@ -18,10 +18,29 @@ constexpr bool kVerbose = true;
 constexpr bool kVerbose = false;
 #endif
 
+std::string example1 = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
+std::string example2 = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\n"
+                       "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc\n"
+                       "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg\n"
+                       "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb\n"
+                       "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea\n"
+                       "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb\n"
+                       "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe\n"
+                       "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef\n"
+                       "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb\n"
+                       "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce";
+
 TEST(Day08, ExampleOnePartOne) {
-  std::string input = "1122";
+  std::string input = example1;
   std::istringstream in(input);
-  EXPECT_EQ(aoc::part1(in, kVerbose), "Not implemented");
+  EXPECT_EQ(aoc::part1(in, kVerbose), "0");
+}
+
+
+TEST(Day08, ExampleTwoPartOne) {
+  std::string input = example2;
+  std::istringstream in(input);
+  EXPECT_EQ(aoc::part1(in, kVerbose), "26");
 }
 
 TEST(Day08, ExampleOnePartTwo) {
